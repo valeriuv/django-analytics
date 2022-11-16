b4dd396a3070dddd07157417b6b864b2720e9903
valeriuv <valeriu.vetiul@gmail.com>
2022-11-16 09:50:35 +0200

    Add reports to DB

--- src/reports/views.py
+++ src/reports/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from profiles.models import Profile
+from django.http import JsonResponse
+from .utils import get_report_image
+from .models import Report
+from django.views.generic import ListView, DetailView
 
 # Create your views here.
+
+
+class ReportListView(ListView):
+    model = Report
+    template_name = 'reports/main.html'
+
+class ReportDetailView(DetailView):
+    model = Report
+    template_name ='reports/detail.html'
+
+
+def create_report_view(request):
+    if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+        name = request.POST.get('name')
+        remarks = request.POST.get('remarks')
+        image = request.POST.get('image')
+
+        img = get_report_image(image)
+
+        author = Profile.objects.get(user=request.user)
+        Report.objects.create(name=name, remarks=remarks, image=img, author=author)
+        return JsonResponse({'msg': 'send'})
+    return JsonResponse({'request': 'not_ajax'})
--- src/sales/static/sales/home.js
+++ src/sales/static/sales/home.js
@@ -1,9 +1,19 @@
 const reportBtn = document.getElementById('report-btn');
 const img = document.getElementById('img');
 const modalBody = document.getElementById('modal-body');
+const reportForm = document.getElementById('report-form');
+const alertBox = document.getElementById('alert-box');
+const reportName = document.getElementById('id_name');
+const reportRemarks = document.getElementById('id_remarks');
+const csrf = document.getElementsByName('csrfmiddlewaretoken')[0].value;
 
-console.log(reportBtn);
-console.log(img);
+const handleAlerts = (type, msg) => {
+    alertBox.innerHTML = `
+        <div class="alert alert-${type}" role="alert">
+            ${msg}
+        </div>
+    `
+}
 
 if (img) {
     reportBtn.classList.remove('not-visible');
@@ -13,4 +23,31 @@ reportBtn.addEventListener('click', () => {
     console.log('Clicked');
     img.setAttribute('class', 'w-100');
     modalBody.prepend(img);
+
+    console.log(img.src);
+
+    reportForm.addEventListener('submit', e=> {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('csrfmiddlewaretoken', csrf);
+        formData.append('name', reportName.value);
+        formData.append('remarks', reportRemarks.value);
+        formData.append('image', img.src);
+
+        $.ajax({
+            type: 'POST',
+            url: '/reports/save/',
+            data: formData,
+            success: function(response){
+                console.log(response);
+                handleAlerts('success', 'Report successfully created.')
+            },
+            error: function(error){
+                console.log(error);
+                handleAlerts('danger', 'Something went wrong...')
+            },
+            processData: false,
+            contentType: false
+        });
+    });
 });
